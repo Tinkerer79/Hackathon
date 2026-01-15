861afbda9b294bcf1899ab7e120c3ebd3c68d888
Ansh Agarwal <anshagarwal000112@gmail.com>
2026-01-15 14:35:47 +0530

    hellp

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 import httpx
 from datetime import datetime
+import os
 
 app = FastAPI(title="🌍 India Disaster Prediction API")
 
@@ -126,4 +127,5 @@ async def get_all():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
